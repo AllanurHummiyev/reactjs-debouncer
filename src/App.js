d28ed36c3ae4c333541c3d24f7244db9c6e79b70
Allanur Hummiyev <allanurhummiyev@gmail.com>
2022-10-10 02:30:23 +0700

    debouncer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import "./App.css";
+import { Component } from "react";
+import { debounce } from "lodash";
 
-function App() {
+class WidgetText extends Component {
+  state = { text: "" };
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} />
+        <textarea value={this.state.text} />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  state = { show: true };
+  handleToggle = debounce(() => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  }, 500);
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleToggle}>Toggle</button>
+        {this.state.show ? <WidgetText /> : null}
+      </div>
+    );
+  }
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +53,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
